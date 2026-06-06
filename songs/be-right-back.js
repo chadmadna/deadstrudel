e81d92732c4e410eb7f72c5f21cddbe0905bb925
irsyad nabil <irsyad.nabil@littlelives.com>
2026-06-06 11:20:27 +0700

    update: brb

--- songs/be-right-back.js
+++ songs/be-right-back.js
@@ -1,14 +1,14 @@
 // @title Be Right Back
 // @by DEADLEADERS
 
-samples('http://localhost:3000/strudel.json')
+samples('http://localhost:3000/strudel.json?v=133')
 setcpm(130 / 4)
 
 _$BASS:
-  note(`<f#1*8!7 [a1 a1 c#2 f#2@2 c#2 f#2 c2]>`) // verse
-  // note(`<f#1*8!4 d2*8!2 c#2*8 [a1 a1 c#2 f#2@2 c#2 f#2 c2]>`) // chorus
+  // note(`<f#1*8!7 [a1 a1 c#2 f#2@2 c#2 f#2 c2]>`) // verse
+  note(`<f#1*8!4 d2*8!2 c#2*8 [a1 a1 c#2 f#2@2 c#2 f#2 c2]>`) // chorus
     .s("brbass").att(0).rel(0.2)
-    .gain(0.4)
+    .gain(0.25)
 
 $BELLS: s("bells/4").bank("00-brb").fit().chop(16).seg(8).gain(0.15)
 
@@ -16,10 +16,10 @@ _$PADS:
   note(`<f#1,f#2>`) // intro
   // stack(note(`<[f#1,f#2]>`), note(`<[- - [d3,c4]@6] [[c3,e4]@2 d3 [c3,c#4]@2 e3 [c3,c4]@2]>`).vel(.7)) // chorus
   // stack(note(`<[f#1,f#2]!4 [d1,d2]!2 [c#1,c#2] [a1,a2]>`), note(`<[- - [d3,c4]@6] [[c3,e4]@2 d3 [c3,c#4]@2 e3 [c3,c4]@2]>`).vel(.7)) // chorus
-  // chord(`<Bm DM7 F#m A2>`).slow(2).anchor("B2").voicing().vel(.5) // ada yg ngomong dari surga
-    // stack(
-    //   chord(`<F C G Am F Em Am@2>`).anchor("F3").voicing().vel(.5),
-    // // note(`c3@4 d3@3 c3@5 e3@3 d3@5 f3@3 e3@2 c3@5 c3 b3@5 a3@4 g3@5 c3@2 b3@6 c4 b3 a3@5 d3@3 c3`).slow(8),
+  // chord(`<Bm DM7 F#m A2>`).slow(2).anchor("B2").voicing().vel(.7) // ada yg ngomong dari surga
+    // stack( // lagu klasik
+    //   chord(`<DM7 A2 E F#m DM7 C#m F#m@2>`).anchor("F3").voicing().vel(.5),
+    //   note(`a3@4 b3@3 a3@5 c#4@3 b3@5 d4@3 c#4@2 a3@5 a3 g#4@5 f#4@4 e4@5 a3@2 g#4@6 a4 g#4 f#4@5 b3@3 a3`).trans(-12).slow(8),
     // )
     .s("deadpad").att(0).rel(1)
     .gain(0.3)
@@ -27,17 +27,17 @@ _$PADS:
 _$LEAD:
   note(`<[- - [d2,c3] [c2,d3] - c2 [d2,c3] -] [[c2,e3] - - - d2 - [c2,c#3] - e2 - c2 - c3@2 -@2]>`)
     .s("deadbrass")
-    .lpf(8000).lpq(6)
+    .hpf(500).lpf(12000)
     .rel(0.3).delay(0.9)
-    .gain(0.3)
+    .gain(0.35)
 
-_$DRUMS:
+$DRUMS:
 stack(
   // // main
-  note("<[c2 c2*2 - c2*2 [c2 c2] [- c2] - c2*2]!3 [c2 c2*2 - c2*2 c2 [- c2] - c2]>").s("bd").slow(2).superimpose(x => x.midi()),
-  note("<[- c#2]!7 [- c#2*2 c#2 -@2 c#2 -@2]>").s("sd").superimpose(x => x.midi()),
-  s("<hh*16!7 [hh*8 -]>").vel(`.5 .2`.fast(8)).slow(2),
-  s("<[- oh]!7 [oh -@2 oh -@2 oh -]>").begin(.1).vel(.8),
+  // note("<[c2 c2*2 - c2*2 [c2 c2] [- c2] - c2*2]!3 [c2 c2*2 - c2*2 c2 [- c2] - c2]>").s("bd").slow(2).superimpose(x => x.midi()),
+  // note("<[- c#2]!7 [- c#2*2 c#2 -@2 c#2 -@2]>").s("sd").superimpose(x => x.midi()),
+  // s("<hh*16!7 [hh*8 -]>").vel(`.5 .2`.fast(8)).slow(2),
+  // s("<[- oh]!7 [oh -@2 oh -@2 oh -]>").begin(.1).vel(.8),
 
   // // break
   // note("c2 c2*2 - c2*2 [c2 c2] [- c2] - c2*2").vel("1 .7 1 .7").s("bd").slow(2),
@@ -63,7 +63,8 @@ $NOISE: s("deadfx_noise:1").loopAt(8).chop(64).seg(8).gain(0.1)
 _$VONY: s("vony_sid")
   .bank("00-brb").loopAt(32).chop(64).seg(2)
   .o(2).room(0.8).delay(0.3).delays(0.3).delayfb(0.8)
-  .gain(0.33)
+  .speed(.014).rib("0 | 0.5 | 1.25".fast(2), .5)
+  .gain(0.3)
 
 all(
   x => x?.compressor("-10:10:.1:.1:.5")
@@ -88,7 +89,7 @@ solid(0)
       .posterize(posRand.ease("easeInOutQuad").fast(1.4)),
   )
   .blend(src(s0).modulate(o0).scrollY(-0.1).scale(1, 1, 1.5).rotate(-11), 0.1)
-  // .add(src(o1).mask(shape(100, 0.5, 0.9)).scale(1, height / width), 0.7)  // midi to hydra goodness..
+  .add(src(o1).mask(shape(100, 0.5, 0.9)).scale(1, height / width), 0.7)
   .out(o0)
 
 // MIDI trigs and scenes
