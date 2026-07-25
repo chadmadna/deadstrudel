0ce25f4272966b58517e9f6b0a6eef816bef96da
irsyad nabil <irsyad.nabil@littlelives.com>
2026-07-25 15:10:09 +0700

    update: strudel.json, spa, spill-fill

--- songs/separation-anthem.js
+++ songs/separation-anthem.js
@@ -9,8 +9,8 @@ $NOISE: stack(
   s("deadfx_noise:1").pan(.5),
   s("deadfx_noise:2").pan(.1),
 )
-  .loopAt(8).chop(128).seg(32)
-  .gain(slider(0, 0, 0.2, 0.001))
+  .loopAt(8).chop(128).seg(32).o(2)
+  .gain(slider(0.07, 0, 0.15, 0.001))
 
 _$GUITAR:
   // s("guitar_intro")
@@ -18,11 +18,11 @@ _$GUITAR:
   // s("guitar_verse1")
   // s("guitar_verse2")
   // s("guitar_chorus")
-    .bank("06-spa").loopAt(16).chop(32).seg(4).vel(0.7)
-    .diode(".5:.8").hpf(400)
-    .gain(slider(0.3, 0, 0.3, 0.05))
+    .bank("06-spa").loopAt(16).chop(128).seg(8).vel(0.7).o(2)
+    .diode(".5:.8").hpf(200)
+    .gain(slider(0.35, 0, 0.35))
 
-$BASS:
+_$BASS:
   // s("bass_intro")
   s("bass_main")
   // s("bass_verse")
@@ -30,61 +30,66 @@ $BASS:
     .bank("06-spa").loopAt(16).chop(64).seg(4)
     .vel(0.7)
     .chebyshev(".2:.7").o(2)
-    .gain(slider(0.38, 0, 0.38, 0.05))
+    .gain(slider(0.45, 0, 0.45))
 
-_$PADS:
-  note("[e1,[f#1@3 a#1]]!3 [[e1,f#1]@3 -]").slow(16) // intro
-  // note("e0,f#1,[[e2@3 f#2 g2@2]@3 [a#0,a#2]]").slow(4) // bridge
+$PADS:
+  // note("[e1,[f#1@3 a#1]]!3 [[e1,f#1]@3 -]").slow(16) // intro
+  note("e0,f#1,[[e2@3 f#2 g2@2]@3 [a#0,a#2]]").slow(4) // bridge
     .s("deadpad").o(2)
     .hpf(400).att(0.3).rel(0.5)
-    .gain(0.35)
+    .gain(0.45)
 
-$DRUMS:
-  /** INTRO **/
-  // stack(
-  //   s("[bd*4]!14 [bd!3 [bd bd*2]] bd").slow(16).vel(.9).hpf(70).hpq(5),
-  //   s("[- [-!5 ht lt ht] - [-!5 sd lt ht]]!3 [- [-!5 ht lt ht] - [sd,lt]]").slow(16).vel(.7),
-  //   s("sear").loopAt(2).chop(32).seg(32).rel(0).mask("0!15 1".slow(16)).vel(saw.range(0.4, 1.3).seg(32)),
-  // )
-  /** MAIN **/
-  // stack(
-  //   s("[bd [- bd]!2 -]!15 <[- bd - bd]>").slow(16).vel(1).hpf(50).hpq(6),
-  //   s("[[- sd]*2]!15 <[[- sd]*2]>").slow(16).vel(1),
-  //   s("[[ht -]*4]!15 <[[ht -]*2 [lt*2 ht lt ht]]>").slow(16).vel(.6).dec(.6).pan(.4),
-  //   s("[[- ht]*4]!15 <[[- ht]*2 -]>").slow(16).vel(.4).dec(.6).pan(.6),
-  // )
-  /** CHORUS **/
-  stack(
+let drumPats = {
+  intro: stack(
+    s("[bd*4]!14 [bd!3 [bd bd*2]] bd").slow(16).vel(.9).hpf(70).hpq(5),
+    s("[- [-!5 ht lt ht] - [-!5 sd lt ht]]!3 [- [-!5 ht lt ht] - [sd,lt]]").slow(16).vel(.7),
+    s("sear").loopAt(2).chop(32).seg(32).rel(0).mask("0!15 1".slow(16)).vel(saw.range(0.4, 1.3).seg(32)),
+  ),
+  main: stack(
+    s("[bd [- bd]!2 -]!15 <[- bd - bd]>").slow(16).vel(1).hpf(50).hpq(6),
+    s("[[- sd]*2]!15 <[[- sd]*2]>").slow(16).vel(1),
+    s("[[ht -]*4]!15 <[[ht -]*2 [lt*2 ht lt ht]]>").slow(16).vel(.6).dec(.6).pan(.4),
+    s("[[- ht]*4]!15 <[[- ht]*2 -]>").slow(16).vel(.4).dec(.6).pan(.6),
+    // s("-!15 sear").loopAt(2).rel(0.5).slow(8).vel(1),
+  ),
+  chorus: stack(
     s("<[[bd [- bd] -@2]!3 [bd [- bd]!2 [- bd]]] [[[- bd]!4]!3 [[- bd]!2 [bd!3 -] [- bd]]]>").slow(4).vel(1).hpf(70).hpq(5),
     s("<[[- sd]!3 [sd*2 - sd sd]] [[- sd]!8]>").slow(4).vel(1),
     s("<[[ht -]!16] ->").slow(4).vel(.6).dec(.6).pan(.4),
     s("<[[- lt]*16] [- oh!15]>").slow(4).vel(.4).dec(.6).pan(.6),
+  ),
+  bridge: stack(
+    // s("bd*4").vel(1).hpf(70).hpq(5).duck(2).duckdepth(.3).datt(.25),
+    // s("[- hh]*4").vel(.35),
+    s("[- sd]*2").vel(1),
+    s("[boom,bd] [- [boom,bd]@2] - [boom,bd] - [boom,bd]!3").dec(.3).vel(.9),
   )
-  /** BRIDGE **/
-  // stack(
-  //   s("bd*4").vel(.9).hpf(70).hpq(5).duck(2).duckdepth(.2).datt(.25),
-  //   s("[- hh]*4").vel(.4),
-  //   s("[- sd]*2").vel(1)
-  // )
+}
+
+_$DRUMS:
+  drumPats.main
     .bank("deadrums")
-    .hpf(100)
-    .gain(1)
+    .gain(1.2)
+
+_$SPILL: stack(
+  s("spillfill:48").vel(.5).loopAt(1).hpf(400),
+)
 
 $HIT:
-  // s("hit").slow(16) // verse, main
-  s("hit -@30 hit -@32").slow(8) // chorus
+  s("hit").slow(16) // verse, main
+  // s("hit -@30 hit -@32").slow(8) // chorus
     .bank("04-avy")
     .delay(0.5).room(0.5)
-    .gain(0.25)
+    .gain(0.4)
 
 _$BREAKS: s("riffin").loopAt(2).chop(16).segment(8)
   .pickF("<pat!7 <fillA fillB>>", {
     pat: x => x
       .when("0 1!3", x => x
-        .sometimesBy(.7, x => x.rib("0 | 3".div(8), .75))
+        .sometimesBy(1, x => x.rib("0 | 3".div(8), .75))
        )
       .when("0 1!7".slow(2), x => x
-        .sometimesBy(.1, wchoose(
+        .sometimesBy(.5, wchoose(
           [x => x.ply("4 | 6"), 3],
           [x => x.scrub("{2!4}%16".div(16)).speed(".3 .33 .36 .39".fast(4)), 1]
         ))
@@ -92,10 +97,10 @@ _$BREAKS: s("riffin").loopAt(2).chop(16).segment(8)
     fillA: x => x.scrub("{0!3 0*2 2!2 2*3 2*6}%8".div(16)),
     fillB: x => x.scrub(stepcat([1, "0 0"], [3, run(32).div(32).add(4).div(32)])),
   })
-  .sinefold("1:.2")
+  .sinefold("1:.3")
   .lpf(slider(140,0, 140).pow(2)).lpq(3)
   .bank("yaxu-clean-breaks")
-  .gain(.52)
+  .gain(.5)
 
 _$SPARSE: s("riffin").loopAt(2).chop(16).segment(8)
   .pickF("<pat!7 fill>".slow(2), {
@@ -104,14 +109,14 @@ _$SPARSE: s("riffin").loopAt(2).chop(16).segment(8)
     fill: x => x.scrub("{0@3 ~ 4 ~!3 0@3 ~!4 4 0@3 ~ 4 ~!2 6 0 ~ 0!2 3!3 2*2}%8".div(16))
       .layer(x => x.juxBy(0.2, press)),
   })
-  .rel(.2).vel(.6)
-  .sinefold("1:.25")
+  .rel(.2).vel(.8)
+  .sinefold("1:.3")
   .delay(0.3).delayfb(.5)
   .lpf(slider(140,0, 140).pow(2)).lpq(3)
   .bank("yaxu-clean-breaks")
   .gain(.5)
 
-// all((x) => x.compressor("-10:10:.1:.1:.5")).postgain()
+all((x) => x.postgain(.9))
 
 await initHydra()
 
